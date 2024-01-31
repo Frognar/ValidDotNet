9e74e767d48a2dad6c4cc83d40ea7aa318cd6e38
sebastian.przyszlak <sebastian.przyszlak@ex7.pl>
2024-01-31 16:56:34 +0100

    Use primary constructor for Validator

--- src/ValidDotNet/Validator.cs
+++ src/ValidDotNet/Validator.cs
@@ -2,12 +2,8 @@
 
 namespace Frognar.ValidDotNet;
 
-public class Validator<T> {
-  readonly ImmutableList<(Func<T, bool> isInvalid, string errorMessage)> validators;
-
-  public Validator(params (Func<T, bool> isInvalid, string errorMessage)[] validators) {
-    this.validators = validators.ToImmutableList();
-  }
+public class Validator<T>(params (Func<T, bool> isInvalid, string errorMessage)[] validators) {
+  readonly ImmutableList<(Func<T, bool> isInvalid, string errorMessage)> validators = validators.ToImmutableList();
 
   public ValidationResult Validate(T item) {
     return validators.Any(v => v.isInvalid(item))
