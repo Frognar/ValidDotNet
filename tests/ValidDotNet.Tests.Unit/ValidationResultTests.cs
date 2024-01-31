94cf8f2af860ebc9901691db1bcce8b1724d6ab2
frognar <sebastian.przyszlak@gmail.com>
2024-01-31 07:14:15 +0100

    Refactor tests

--- tests/ValidDotNet.Tests.Unit/ValidationResultTests.cs
+++ tests/ValidDotNet.Tests.Unit/ValidationResultTests.cs
@@ -4,32 +4,39 @@ public class ValidationResultTests {
   [Fact]
   public void IsValidWithoutErrors() {
     ValidationResult result = new();
-    result.IsValid.Should().Be(true);
+    result.IsValid.Should().BeTrue();
   }
 
   [Fact]
   public void IsValidWhenEmptyErrors() {
     ValidationResult result = new([]);
-    result.IsValid.Should().Be(true);
+    result.IsValid.Should().BeTrue();
+  }
+
+  [Fact]
+  public void HasNoErrorsWhenNoErrorAdded() {
+    ValidationResult result = new();
+    result.Errors.Should().BeEmpty();
   }
 
   [Fact]
   public void IsInvalidWithErrors() {
     ValidationResult result = new(["error"]);
-    result.IsValid.Should().Be(false);
+    result.IsValid.Should().BeFalse();
   }
 
   [Fact]
   public void HasErrorsWhenCreatedWithErrors() {
     ValidationResult result = new(["error"]);
-    result.Errors.Count.Should().Be(1);
+    result.Errors.Should().HaveCount(1);
+    result.Errors.Should().Contain("error");
   }
 
   [Fact]
   public void IsInvalidWhenErrorAdded() {
     ValidationResult result = new();
     result = result.AddError("error");
-    result.IsValid.Should().Be(false);
+    result.IsValid.Should().BeFalse();
   }
 
   [Fact]
