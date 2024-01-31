2b1eb299bb4f1156b7bf7ab0c3cf0257bd7e8d99
sebastian.przyszlak <sebastian.przyszlak@ex7.pl>
2024-01-31 16:41:08 +0100

    Add Validate method

--- src/ValidDotNet/Validator.cs
+++ src/ValidDotNet/Validator.cs
@@ -1,6 +1,12 @@
-﻿namespace Frognar.ValidDotNet;
+﻿using System.Diagnostics;
+
+namespace Frognar.ValidDotNet;
 
 public class Validator<T> {
   public Validator((Func<T, bool> isInvalid, string errorMessage) validator) {
+  }
+
+  public ValidationResult Validate(T item) {
+    return ValidationResult.valid;
   }
 }
--- tests/ValidDotNet.Tests.Unit/ValidatorTests.cs
+++ tests/ValidDotNet.Tests.Unit/ValidatorTests.cs
@@ -5,4 +5,10 @@ public class ValidatorTests {
   public void CreateValidatorForOddInts() {
     Validator<int> validator = new((i => i % 2 == 0, "must be odd"));
   }
+
+  [Fact]
+  public void OddIntsAreConsideredValid() {
+    Validator<int> validator = new((i => i % 2 == 0, "must be odd"));
+    validator.Validate(1).IsValid.Should().BeTrue();
+  }
 }
